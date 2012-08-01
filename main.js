c555acf509f6da84653a5972d405f1292a320215
Thomas Buckley <tbuckley@Tbuckley MacBook Pro.(none)>
2012-07-31 21:16:21 -0400

    Added tap function

--- main.js
+++ main.js
@@ -1,13 +1,53 @@
-$(document).ready(function() {
-	var body = document.getElementsByTagName('html')[0];
-	var logTouches = function(e) {
-		console.log(e);
-		e.preventDefault();
+function getTouchIds(touches) {
+	var ids = [];
+	for(var i = 0; i < touches.length; i++) {
+		ids.push(touches[i].identifier);
+	}
+	return ids;
+}
+function getChangedIds(e) {return getTouchIds(e.changedTouches);}
+function getTargetIds(e) {return getTouchIds(e.targetTouches);}
+function getTouchIds(e) {return getTouchIds(e.touches);}
+
+function makeLookupTable(arr) {
+	var table = {};
+	for(var val in arr) {
+		table[val] = true;
+	}
+	return table;
+}
+
+function getNewTouches(e) {
+	var changed = makeLookupTable(getChangedTouches(e));
+	var target = getTargetIds(e);
+	var newTouches = [];
+	for(var id in target) {
+		if(id in changed) {
+			newTouches.push(id);
+		}
+	}
+	return newTouches;
+}
+
+(function($) {
+	$.fn.tap = function(handler) {
+		this.addEventListener("touchstart", function(e) {
+			console.log(JSON.stringify(getNewTouches(e)));
+		}, false);
 	};
-	body.addEventListener("touchstart", logTouches, false);
-	body.addEventListener("touchend", logTouches, false);
-	body.addEventListener("touchmove", logTouches, false);
-	body.addEventListener("touchenter", logTouches, false);
-	body.addEventListener("touchleave", logTouches, false);
-	body.addEventListener("touchcancel", logTouches, false);
+})(jQuery);
+
+$(document).ready(function() {
+	// var body = document.getElementsByTagName('html')[0];
+	// var logTouches = function(e) {
+	// 	console.log(e);
+	// 	e.preventDefault();
+	// };
+	// body.addEventListener("touchstart", logTouches, false);
+	// body.addEventListener("touchend", logTouches, false);
+	// body.addEventListener("touchmove", logTouches, false);
+	// body.addEventListener("touchenter", logTouches, false);
+	// body.addEventListener("touchleave", logTouches, false);
+	// body.addEventListener("touchcancel", logTouches, false);
+	$("#blue").tap();
 });
